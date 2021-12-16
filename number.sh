e765bd30143ee97e50823d801eb3fa222f1d49c7
Rahul Khandelwal <71495581+RahulKhandelwal076@users.noreply.github.com>
2021-12-16 10:38:29 +0530

    Update number.sh

--- number.sh
+++ number.sh
@@ -1,7 +1,7 @@
-file_path="/home/bmsce/Desktop/number.sh"
-lines=`wc --lines < $file_path`
-words=`wc --word < $file_path`
-characters=`wc --c < $file_path`
+path="/home/bmsce/Desktop/number.sh"
+lines=`wc --lines < $path`
+words=`wc --word < $path`
+characters=`wc --c < $path`
 echo "Number of lines: $lines"
 echo "Number of words: $words"
 echo "Number of characters: $characters"
